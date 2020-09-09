253edac004e3d6d86b01e54500e1733c068cbc30
kyab <kyossi212@gmail.com>
2020-09-09 21:31:46 +0900

    Initial Commit

---
+++ Late5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Late5
+//
+//  Created by kyab on 2020/09/09.
+//  Copyright © 2020 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Late5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Late5
+//
+//  Created by kyab on 2020/09/09.
+//  Copyright © 2020 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
